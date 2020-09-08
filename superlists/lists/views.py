d3490462815825209be759b30eecdc38e3c57389
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-07 23:54:58 -0300

    Não Salvar items em Branco a cada Requisição

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from lists.models import Item
 
 def home_page(request):
-
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+    new_item_text = ''
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
 
     return render(request, 'home.html', {
-        'new_item_text': item.text
+        'new_item_text': new_item_text
     })
