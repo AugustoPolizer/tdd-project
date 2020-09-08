05257a29ae5f30e137ac9186273666218af72d50
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-08 00:03:33 -0300

    Refatorando o Teste de Unidade

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -18,8 +18,11 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.['location'], '/')
+        self.assertEqual(response['location'], '/')
 
     def test_only_saves_items_when_neessary(self):
         self.client.get('/')
