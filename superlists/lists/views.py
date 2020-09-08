1311bfbabf70e7268e97b9840cd789048afa960e
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-07 23:52:57 -0300

    Salvando o POST no Banco de Dados

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from lists.models import Item
 
 def home_page(request):
+
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': item.text
     })
