7736969d281db0e6bf0dfdae49e68b2dbe275042
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-09 01:33:05 -0300

    Implementando o Novo Projeto: View para Adicionar Items em uma Lista Existente

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -75,3 +75,31 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -13,3 +13,8 @@ def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
     return redirect(f'/lists/{my_list.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
