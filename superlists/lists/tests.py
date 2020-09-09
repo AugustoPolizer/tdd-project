89b4117fc2401479599d476aa5e182c0338939ab
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-09 00:41:44 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 5

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -11,11 +11,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html') 
 
-
-    def test_only_saves_items_when_neessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 
