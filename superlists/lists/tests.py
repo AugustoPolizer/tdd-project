baaea436ad73fe082e5d86a1696818384af6e2f8
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-09 01:30:55 -0300

    Implementando o Novo Projeto: Cada Lista Deve Ter Uma URL

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -43,19 +43,24 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        my_list = List.objects.create()
+        response = self.client.get(f'/lists/{my_list.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-    def test_displays_all_items(self):
-        my_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
-        Item.objects.create(text='itemey 1', list=my_list)
-        Item.objects.create(text='itemey 2', list=my_list)
-
-        response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
 class NewListTest(TestCase):
 
@@ -68,6 +73,5 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,11 +4,12 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    my_list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=my_list)
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{my_list.id}/')
