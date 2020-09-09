3bed30167e3f926d11f67d1d57deaf6fc4bcced5
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-08 23:34:19 -0300

    Melhoria 2:Removendo Sleeps para Evitar Não Determinismo

---
+++ superlists/functional_tests/tests.py
@@ -0,0 +1,71 @@
+# enconding: utf-8
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from django.test import LiveServerTestCase
+import time
+
+MAX_WAIT = 5
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+            start_time = time.time()
+            while True:  
+                try:
+                    table = self.browser.find_element_by_id('id_list_table')  
+                    rows = table.find_elements_by_tag_name('tr')
+                    self.assertIn(row_text, [row.text for row in rows])
+                    return  
+                except (AssertionError, WebDriverException) as e:  
+                    if time.time() - start_time > MAX_WAIT:  
+                        raise e  
+                    time.sleep(0.5)
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+    
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get(self.live_server_url)
+
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
+
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')        
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        self.wait_for_row_in_list_table('2: Estudar testes de unidade')
+
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+        # Satisfeita, ela vai dormir
+
