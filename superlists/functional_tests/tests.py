085984ef13148f5d7b2886f8d906f12399e2a462
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-09 00:39:08 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 4

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -70,7 +70,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisfeita, ela vai dormir
 
 
-     def test_multiple_users_can_start_lists_at_different_urls(self):
+    def test_multiple_users_can_start_lists_at_different_urls(self):
         # Maria começa uma nova lista
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
