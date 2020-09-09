e336fd41e73c823686e2291a97b060be83ae4dd9
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-09 00:47:31 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -11,16 +11,24 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html') 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
         first_item = Item()
         first_item.text = 'O primeiro item'
+        first_item.list = my_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'O segundo item'
+        second_item.list = my_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -28,8 +36,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
         self.assertEqual(second_saved_item.text, 'O segundo item')
-
+        self.assertEqual(second_saved_item.list, my_list)
 
 class ListViewTest(TestCase):
 
