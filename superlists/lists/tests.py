25c8413a5b6669003ada8dd6a75fe9cc05c735c1
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-09 01:21:21 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -48,25 +48,26 @@ class ListViewTest(TestCase):
 
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        my_list = List.objects.create()
+
+        Item.objects.create(text='itemey 1', list=my_list)
+        Item.objects.create(text='itemey 2', list=my_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
-
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
-
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    my_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=my_list)
     return redirect('/lists/the-only-list-in-the-world/')
