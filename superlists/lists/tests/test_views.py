e145a67654374d7078ca1084b187f1357d44726c
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-10-18 13:48:02 -0300

    Entrada inválida não deve ser salva no BD

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -38,6 +38,11 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
 class NewListTest(TestCase):
+    
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
