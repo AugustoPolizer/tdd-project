5fb3577e6dff86c905ca7c66e52cabeff321eac3
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-10-18 13:18:08 -0300

    Refatorando o Teste de Unidade

---
+++ superlists/lists/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.list = my_list
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.list = my_list
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
+        self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, my_list)
+
--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,8 +1,4 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -11,34 +7,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html') 
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        my_list = List()
-        my_list.save()
-
-        first_item = Item()
-        first_item.text = 'O primeiro item'
-        first_item.list = my_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'O segundo item'
-        second_item.list = my_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, my_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'O primeiro item')
-        self.assertEqual(first_saved_item.list, my_list)
-        self.assertEqual(second_saved_item.text, 'O segundo item')
-        self.assertEqual(second_saved_item.list, my_list)
 
 class ListViewTest(TestCase):
 
