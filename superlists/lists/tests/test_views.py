6f5bb313adcc7ba5f14f594cd3a1732e55a270a9
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-10-18 14:17:49 -0300

    De volta ao teste funcional - PARTE 2

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -5,6 +5,7 @@ from django.utils.html import escape
 class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
+
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html') 
 
@@ -12,17 +13,20 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_passes_correct_list_to_template(self):
+
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
     def test_uses_list_template(self):
+
         my_list = List.objects.create()
         response = self.client.get(f'/lists/{my_list.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_only_items_for_that_list(self):
+        
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
         Item.objects.create(text='itemey 2', list=correct_list)
@@ -38,6 +42,7 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
+
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
@@ -52,6 +57,7 @@ class ListViewTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
     def test_redirects_to_list_view(self):
+
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
@@ -60,6 +66,18 @@ class ListViewTest(TestCase):
             data={'item_text': 'A new item for an existing list'}
         )
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_validation_errors_end_up_on_lists_page(self):
+
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 class NewListTest(TestCase):
     
     def test_invalid_list_items_arent_saved(self):
